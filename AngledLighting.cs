1713c0424d4dcd4672421e585c8957964b762655
Goodlyay <zackxv7@yahoo.com>
2017-04-20 22:11:30 -0700

    Make AngledLighting go diagonal on both axis

--- AngledLighting.cs
+++ AngledLighting.cs
@@ -33,72 +33,83 @@ namespace ClassicalSharp.Map {
 			oneY = width * length;
 			
 			heightmap = new short[width * length];
-			blockers = new int[(width + height) * length];
+			
+			blockers = new int[(width + height) * (length + height)];
 			CalcLightDepths(0, 0, width, length);
 		}
 		
 		
-        public void CalcLightDepths(int xStart, int zStart, int xWidth, int zDepth) { //flag1
-            
+        public void CalcLightDepths(int xStart, int zStart, int xWidth, int zLength) {
+            //xStart and zStart are zero.
         	World map = game.World;
         	
-        	xStart += height;
-        	if (xWidth == width) {
+        	xStart += height; //add xStart to the height of the map because
+        	if (xWidth == width) { //Since xWidth starts the same as width, this always happens at first
         		xWidth += height;
         		xStart -= height;
+        		//xStart is zero again...
+        		//xWidth is now equal to the height of the map
         	}
-        	int widthRow = width + height;
-            for (int x = xStart; x < xStart + xWidth; ++x) {
-                for (int z = zStart; z < zStart + zDepth; ++z) {
-                    int oldY = blockers[x + z * widthRow];
+        	
+        	zStart += height;
+        	if (zLength == length) {
+        		zLength += height;
+        		zStart -= height;
+        	}
+        	
+        	//the size of the lightmap in each dimension
+        	int xExtent = width + height;
+            int zExtent = length + height;
+        	
+            for (int x = xStart; x < xStart + xWidth; ++x) { //from 0 to the width + height of the map
+                for (int z = zStart; z < zStart + zLength; ++z) { //from 0 to the length of the map
+        	        
+                    int oldY = blockers[x + z * xExtent];
      
-                    int y = height -1;
+                    int y = height -1; //-1 because it starts at 0
                     int xD = x + height -1;
-                    if (xD >= widthRow) {
-                    	int xOver = xD - (widthRow -1);
-                    	y -= xOver;
-                    	xD -= xOver;
+                    int zD = z + height -1;
+                    
+                    int xOver = 0; //how far past the edge of the map is it?
+                    int zOver = 0; 
+                    
+
+                    if (xD >= xExtent) {
+                    	xOver = xD - (xExtent -1);
                     }
+                    if (zD >= zExtent) {
+                    	zOver = zD - (zExtent -1);//how far past the edge of the map is it?
+                    }
+                    int maxOver = Math.Max(xOver, zOver);
+                    //pushing y and x and z back to the edge of the map
+                    y -= maxOver;
+                    xD -= maxOver;
+                    zD -= maxOver; 
+                    
                     xD -= height;
-                    while (y > 0 && xD >= 0 && xD < width && !info.BlocksLight[map.GetBlock(xD, y, z)]) {
+                    zD -= height;
+                    
+                    while (y > 0 &&
+                           xD >= 0 &&
+                           xD < width &&
+                           zD >= 0 &&
+                           zD < length &&
+                           !info.BlocksLight[map.GetBlock(xD, y, zD)]
+                          ) {
+                        
                         --y;
                         --xD;
+                        --zD;
                     }
-                    if (xD < 0) {
+                    
+                    if (xD < 0 || zD < 0) {
                     	y = oldY;
                     }
-                    blockers[x + z * widthRow] = y; //blockers becomes y
+                    blockers[x + z * xExtent] = y;
+                    
                 }
             }
         }
-		
-		/*void CalcLightDepths() {
-			int i = 0;
-			World map = game.World;
-			for (int z = 0; z < length; z++)
-				for (int x = 0; x < width; x++)
-			{
-				for (int y = height - 1; y >= heightmap[i]; y--) {
-					if (info.BlocksLight[map.GetBlock(x, y, z)]) {
-						CastShadow(x, y, z);
-						break;
-					}
-				}				
-				i++;
-			}
-		}
-		
-		void CastShadow(int x, int y, int z) {
-			const int dirX = 1, dirZ = 1;
-			
-			while (x >= 0 && z >= 0 && x < width && z < length) {
-				int hIndex = z * width + x;
-				heightmap[hIndex] = (short)Math.Max(y, heightmap[hIndex]);
-				
-				y--;
-				x += dirX; z += dirZ;
-			}
-		}*/
 		
 		public override void Init(Game game) {
 			game.WorldEvents.EnvVariableChanged += EnvVariableChanged;
@@ -142,8 +153,12 @@ namespace ClassicalSharp.Map {
 		
 		// Outside colour is same as sunlight colour, so we reuse when possible
 		public override bool IsLit(int x, int y, int z) {
-            return !(x >= 0 && y >= 0 && z >= 0 && x < width && y < height
-            && z < length) || y >= blockers[(x +height -y) + z * (width + height)]; //flag2
+            return !(x >= 0 &&
+		             y >= 0 &&
+		             z >= 0 &&
+		             x < width &&
+		             y < height &&
+		             z < length) || y >= blockers[(x +height -y) + (z +height -y) * (width + height)];
 		}
 
 		public override int LightCol(int x, int y, int z) {
@@ -173,7 +188,7 @@ namespace ClassicalSharp.Map {
 			if (IsLit(x, y + 1, z)) {
 			    return Outside;
 			}
-			return shadowXSide;
+			return shadowZSide;
 		}
 		
 		public override int LightCol_YBottom_Fast(int x, int y, int z) {
