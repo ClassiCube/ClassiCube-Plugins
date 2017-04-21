3ccae9f0057eab320fe17a24ca7834708378b994
UnknownShadow200 <unknownshadow200@gmail.com>
2017-04-21 15:23:06 +1000

    Code cleanup

--- AngledLighting.cs
+++ AngledLighting.cs
@@ -14,7 +14,7 @@ namespace ClassicalSharp.Map {
 		int oneY, shadow, shadowZSide, shadowXSide, shadowYBottom;
 		BlockInfo info;
 		Game game;
-		int[] blockers; 
+		int[] blockers;
 		public override void Reset(Game game) { heightmap = null; blockers = null; }
 		
 		public override void OnNewMap(Game game) {
@@ -38,78 +38,70 @@ namespace ClassicalSharp.Map {
 			CalcLightDepths(0, 0, width, length);
 		}
 		
-		
-        public void CalcLightDepths(int xStart, int zStart, int xWidth, int zLength) {
-            //xStart and zStart are zero.
-        	World map = game.World;
-        	
-        	xStart += height; //add xStart to the height of the map because
-        	if (xWidth == width) { //Since xWidth starts the same as width, this always happens at first
-        		xWidth += height;
-        		xStart -= height;
-        		//xStart is zero again...
-        		//xWidth is now equal to the height of the map
-        	}
-        	
-        	zStart += height;
-        	if (zLength == length) {
-        		zLength += height;
-        		zStart -= height;
-        	}
-        	
-        	//the size of the lightmap in each dimension
-        	int xExtent = width + height;
-            int zExtent = length + height;
-        	
-            for (int x = xStart; x < xStart + xWidth; ++x) { //from 0 to the width + height of the map
-                for (int z = zStart; z < zStart + zLength; ++z) { //from 0 to the length of the map
-        	        
-                    int oldY = blockers[x + z * xExtent];
-     
-                    int y = height -1; //-1 because it starts at 0
-                    int xD = x + height -1;
-                    int zD = z + height -1;
-                    
-                    int xOver = 0; //how far past the edge of the map is it?
-                    int zOver = 0; 
-                    
+		const int dx = -1, dz = -1;
+		public void CalcLightDepths(int xStart, int zStart, int xWidth, int zLength) {
+			//xStart and zStart are zero.
+			World map = game.World;
+			
+			xStart += height; //add xStart to the height of the map because
+			if (xWidth == width) { //Since xWidth starts the same as width, this always happens at first
+				xWidth += height;
+				xStart -= height;
+				//xStart is zero again...
+				//xWidth is now equal to the height of the map
+			}
+			
+			zStart += height;
+			if (zLength == length) {
+				zLength += height;
+				zStart -= height;
+			}
+			
+			//the size of the lightmap in each dimension
+			int xExtent = width + height;
+			int zExtent = length + height;
+			
+			for (int x = xStart; x < xStart + xWidth; ++x) { //from 0 to the width + height of the map
+				for (int z = zStart; z < zStart + zLength; ++z) { //from 0 to the length of the map
+					
+					int oldY = blockers[x + z * xExtent];
+					
+					int y = height -1; //-1 because it starts at 0
+					int xD = x + height -1;
+					int zD = z + height -1;
+					
+					int xOver = 0; //how far past the edge of the map is it?
+					int zOver = 0;
+					
 
-                    if (xD >= xExtent) {
-                    	xOver = xD - (xExtent -1);
-                    }
-                    if (zD >= zExtent) {
-                    	zOver = zD - (zExtent -1);//how far past the edge of the map is it?
-                    }
-                    int maxOver = Math.Max(xOver, zOver);
-                    //pushing y and x and z back to the edge of the map
-                    y -= maxOver;
-                    xD -= maxOver;
-                    zD -= maxOver; 
-                    
-                    xD -= height;
-                    zD -= height;
-                    
-                    while (y > 0 &&
-                           xD >= 0 &&
-                           xD < width &&
-                           zD >= 0 &&
-                           zD < length &&
-                           !info.BlocksLight[map.GetBlock(xD, y, zD)]
-                          ) {
-                        
-                        --y;
-                        --xD;
-                        --zD;
-                    }
-                    
-                    if (xD < 0 || zD < 0) {
-                    	y = oldY;
-                    }
-                    blockers[x + z * xExtent] = y;
-                    
-                }
-            }
-        }
+					if (xD >= xExtent) {
+						xOver = xD - (xExtent -1);
+					}
+					if (zD >= zExtent) {
+						zOver = zD - (zExtent -1);//how far past the edge of the map is it?
+					}
+					int maxOver = Math.Max(xOver, zOver);
+					//pushing y and x and z back to the edge of the map
+					y -= maxOver;
+					xD -= maxOver;
+					zD -= maxOver;
+					
+					xD -= height;
+					zD -= height;
+					
+					while (y > 0 && xD >= 0 && xD < width && zD >= 0 && zD < length &&
+					       !info.BlocksLight[map.GetBlock(xD, y, zD)]) {
+						xD += dx; y -= 1; zD += dz;
+					}
+					
+					if (xD < 0 || zD < 0) {
+						y = oldY;
+					}
+					blockers[x + z * xExtent] = y;
+					
+				}
+			}
+		}
 		
 		public override void Init(Game game) {
 			game.WorldEvents.EnvVariableChanged += EnvVariableChanged;
@@ -153,63 +145,37 @@ namespace ClassicalSharp.Map {
 		
 		// Outside colour is same as sunlight colour, so we reuse when possible
 		public override bool IsLit(int x, int y, int z) {
-            return !(x >= 0 &&
-		             y >= 0 &&
-		             z >= 0 &&
-		             x < width &&
-		             y < height &&
-		             z < length) || y >= blockers[(x +height -y) + (z +height -y) * (width + height)];
+			return !(x >= 0 && y >= 0 && z >= 0 && x < width && y < height && z < length)
+				|| y >= blockers[(x +height -y) + (z +height -y) * (width + height)];
 		}
 
 		public override int LightCol(int x, int y, int z) {
 			//return y > GetLightHeight(x, z) ? Outside : shadow;
-			if (IsLit(x, y + 1, z)) {
-			    return Outside;
-			}
-			return shadowZSide;
+			return IsLit(x, y + 1, z) ? Outside : shadowZSide;
 		}
 		
 		public override int LightCol_ZSide(int x, int y, int z) {
-			if (IsLit(x, y, z)) {
-			    return OutsideZSide;
-			}
-			return shadowXSide;
-		}
-		
+			return IsLit(x, y, z) ? OutsideZSide : shadowXSide;
+		}		
 
 		public override int LightCol_Sprite_Fast(int x, int y, int z) {
-			if (IsLit(x, y, z)) {
-			    return Outside;
-			}
-			return shadowXSide;
+			return IsLit(x, y, z) ? Outside : shadowXSide;
 		}
 		
 		public override int LightCol_YTop_Fast(int x, int y, int z) {
-			if (IsLit(x, y + 1, z)) {
-			    return Outside;
-			}
-			return shadowZSide;
+			return IsLit(x, y + 1, z) ? Outside : shadowZSide;
 		}
 		
 		public override int LightCol_YBottom_Fast(int x, int y, int z) {
-			if (IsLit(x, y, z)) {
-			    return OutsideYBottom;
-			}
-			return shadowYBottom;
+			return IsLit(x, y, z) ? OutsideYBottom : shadowYBottom;
 		}
 		
 		public override int LightCol_XSide_Fast(int x, int y, int z) {
-			if (IsLit(x, y, z)) {
-			    return OutsideXSide;
-			}
-			return shadowXSide;
+			return IsLit(x, y, z) ? OutsideXSide : shadowXSide;
 		}
 		
 		public override int LightCol_ZSide_Fast(int x, int y, int z) {
-			if (IsLit(x, y, z)) {
-			    return OutsideZSide;
-			}
-			return shadowXSide;
+			return IsLit(x, y, z) ? OutsideZSide : shadowXSide;
 		}
 		
 		
