61c83cb13b719df95477b2de04fab6435e3371ee
UnknownShadow200 <unknownshadow200@gmail.com>
2017-04-29 21:51:52 +1000

    Optimise block lighting for VolumeLighting plugin.
    Still to do:
    - optimise sky lighting
    - optimise chunk updates

--- VolumeLighting.Volume.cs
+++ VolumeLighting.Volume.cs
@@ -14,24 +14,29 @@ namespace VolumeLightingPlugin {
 			int oneY = width * length, maxY = height - 1;
 			byte[] blocks = game.World.blocks;
 			
+			FastQueue queue = new FastQueue(32 * 32 * 32);
+			
 			for( int z = startZ; z < endZ; z++ ) {
 				int horOffset = startX + (z * width);
 				for( int x = startX; x < endX; x++ ) {
 					int index = (maxY * oneY) + horOffset;
 					horOffset++; // increase horizontal position
 					int lightHeight = CalcHeightAt(x, z);
-						
+					
 					for( int y = maxY; y >= 0; y-- ) {
 						byte curBlock = blocks[index];
-						index -= oneY; // reduce y position
-						
 						//if the current block is in sunlight assign the fullest sky brightness to the higher 4 bits
 						if( (y - 1) > lightHeight ) { lightLevels[x, y, z] = (byte)(maxLight << 4); }
+						
 						//if the current block is fullbright assign the fullest block brightness to the higher 4 bits
-						if( info.FullBright[curBlock] ) { lightLevels[x, y, z] |= maxLight; }
+						if( info.FullBright[curBlock] ) queue.Enqueue(index, 15);
+						
+						index -= oneY; // reduce y position
 					}
 				}
 			}
+			
+			CastBlockLight(queue);
 		}
 		
 		int CalcHeightAt(int x, int z) {
@@ -48,7 +53,10 @@ namespace VolumeLightingPlugin {
 			}
 			return -10;
 		}
-		
+
+		// ================
+		// === SKYLIGHT ===
+		// ================
 		void DoPass(int pass, int startX, int startY, int startZ, int endX, int endY, int endZ) {
 			int index = 0;
 			bool[] lightPasses = new bool[Block.Count];
@@ -111,49 +119,109 @@ namespace VolumeLightingPlugin {
 						}
 					}
 				}
-				
-				int blockLight = lightLevels[x, y, z] & 0x0F;
-				//if the current block is not a light blocker AND the current spot is less than i
-				if( (info.FullBright[curBlock] || !info.BlocksLight[curBlock]) && blockLight == pass ) {
-					//check the six neighbors sky light value,
-					if( y < maxY && blockLight > (lightLevels[x, y+1, z] & 0x0F) ) {
-						if( lightPasses[blocks[index + width * length]] ){
-							lightLevels[x, y+1, z] &= 0xF0; // reset blocklight bits to 0
-							lightLevels[x, y+1, z] |= (byte)(blockLight - 1); // set blocklight bits
-						}
-					}
-					if( y > 0 && blockLight > (lightLevels[x, y-1, z] & 0x0F) ) {
-						if( lightPasses[blocks[index - width * length]] ) {
-							lightLevels[x, y-1, z] &= 0xF0;
-							lightLevels[x, y-1, z] |= (byte)(blockLight - 1);
-						}
-					}
-					if( x < maxX && blockLight > (lightLevels[x+1, y, z] & 0x0F) ) {
-						if( lightPasses[blocks[index + 1]] ) {
-							lightLevels[x+1, y, z] &= 0xF0;
-							lightLevels[x+1, y, z] |= (byte)(blockLight - 1);
-						}
-					}
-					if( x > 0 && blockLight > (lightLevels[x-1, y, z] & 0x0F) ) {
-						if( lightPasses[blocks[index - 1]] ) {
-							lightLevels[x-1, y, z] &= 0xF0;
-							lightLevels[x-1, y, z] |= (byte)(blockLight - 1);
-						}
-					}
-					if( z < maxZ && blockLight > (lightLevels[x, y, z+1] & 0x0F) ) {
-						if( lightPasses[blocks[index + width]] ) {
-							lightLevels[x, y, z+1] &= 0xF0;
-							lightLevels[x, y, z+1] |= (byte)(blockLight - 1);
-						}
-					}
-					if( z > 0 && blockLight > (lightLevels[x, y, z-1] & 0x0F) ) {
-						if( lightPasses[blocks[index - width]] ) {
-							lightLevels[x, y, z-1] &= 0xF0;
-							lightLevels[x, y, z-1] |= (byte)(blockLight - 1);
-						}
-					}
-				}
 				index++; // increase one coord
+			}
+		}
+		
+		// ===================
+		// === BLOCK LIGHT ===
+		// ===================		
+		void CastBlockLight(FastQueue queue) {
+			BlockInfo info = game.BlockInfo;
+			byte[] blocks = game.World.blocks;
+			int maxX = width - 1, maxY = height - 1, maxZ = length - 1;
+			int oneY = width * length;
+			
+			while (queue.count > 0) {
+				int idx;
+				byte light;
+				queue.Dequeue(out idx, out light);
+
+				int x = idx % width;
+				int y = idx / oneY;
+				int z = (idx / width) % length;
+
+				lightLevels[x, y, z] &= 0xF0;
+				lightLevels[x, y, z] |= light;
+				
+				if (light == 1) break; // doesn't cast further
+				light--;
+				
+				if (x > 0 && !info.BlocksLight[blocks[idx - 1]] && light > (lightLevels[x - 1, y, z] & 0x0f)) {
+					queue.Enqueue(idx - 1, light);
+				}
+				if (x < maxX && !info.BlocksLight[blocks[idx + 1]] && light > (lightLevels[x + 1, y, z] & 0x0f)) {
+					queue.Enqueue(idx + 1, light);
+				}
+				if (z > 0 && !info.BlocksLight[blocks[idx - width]] && light > (lightLevels[x, y, z - 1] & 0x0f)) {
+					queue.Enqueue(idx - width, light);
+				}
+				if (z < maxZ && !info.BlocksLight[blocks[idx + width]] && light > (lightLevels[x, y, z + 1] & 0x0f)) {
+					queue.Enqueue(idx + width, light);
+				}
+				if (y > 0 && !info.BlocksLight[blocks[idx - oneY]] && light > (lightLevels[x, y - 1, z] & 0x0f)) {
+					queue.Enqueue(idx - oneY, light);
+				}
+				if (y < maxY && !info.BlocksLight[blocks[idx + oneY]] && light > (lightLevels[x, y + 1, z] & 0x0f)) {
+					queue.Enqueue(idx + oneY, light);
+				}
+			}
+		}
+		
+		class FastQueue {
+			int[] idx_buffer;
+			byte[] light_buffer;
+			int head, tail;
+			public int count;
+
+			public FastQueue(int capacity) {
+				idx_buffer = new int[capacity];
+				light_buffer = new byte[capacity];
+			}
+
+			public void Enqueue(int idx, byte light) {
+				if (count == idx_buffer.Length) {
+					int newSize = (int)(idx_buffer.Length * 2);
+					if (newSize < idx_buffer.Length + 4) {
+						newSize = idx_buffer.Length + 4;
+					}
+					SetCapacity(newSize);
+				}
+				
+				idx_buffer[tail] = idx;
+				light_buffer[tail] = light;
+				
+				tail = (tail + 1) % idx_buffer.Length;
+				count++;
+			}
+
+			public void Dequeue(out int idx, out byte light) {
+				idx = idx_buffer[head];
+				light = light_buffer[head];
+				
+				head = (head + 1) % idx_buffer.Length;
+				count--;
+			}
+
+			void SetCapacity(int capacity) {
+				int[] idx_new = new int[capacity];
+				byte[] light_new = new byte[capacity];
+				
+				if (head < tail) {
+					Array.Copy(idx_buffer, head, idx_new, 0, count);
+					Array.Copy(light_buffer, head, light_new, 0, count);
+				} else {
+					Array.Copy(idx_buffer, head, idx_new, 0, idx_buffer.Length - head);
+					Array.Copy(idx_buffer, 0, idx_new, idx_buffer.Length - head, tail);
+					
+					Array.Copy(light_buffer, head, light_new, 0, light_buffer.Length - head);
+					Array.Copy(light_buffer, 0, light_new, light_buffer.Length - head, tail);
+				}
+				
+				idx_buffer = idx_new; light_buffer = light_new;
+				
+				head = 0;
+				tail = ((count == capacity) ? 0 : count);
 			}
 		}
 	}
