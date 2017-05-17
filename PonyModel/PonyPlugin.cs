6a2bacf24fbeb49244b63007780df025e10dc239
UnknownShadow200 <unknownshadow200@gmail.com>
2017-05-18 08:53:10 +1000

    Make pony plugin actually a plugin

---
+++ PonyModel/PonyPlugin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using ClassicalSharp;
+using ClassicalSharp.GraphicsAPI;
+
+namespace Pony {
+
+	public sealed class Core : Plugin {
+		
+		public string ClientVersion { get { return "0.99.4"; } }
+		
+		public void Dispose() { }
+		
+		public void Init(Game game) {
+			game.ModelCache.RegisterTextures("pony.png");
+			game.ModelCache.Register("pony", "pony.png", new PonyModel(game));
+			
+			// Recreate the modelcache VB to be bigger
+			game.Graphics.DeleteVb(ref game.ModelCache.vb);
+			game.ModelCache.vertices = new VertexP3fT2fC4b[24 * 20];
+			game.ModelCache.vb = game.Graphics.CreateDynamicVb(VertexFormat.P3fT2fC4b,
+			                                                   game.ModelCache.vertices.Length);
+			game.Server.AppName += " + Ponies";
+		}
+		
+		public void Ready(Game game) { }
+		
+		public void Reset(Game game) { }
+		
+		public void OnNewMap(Game game) { }
+		
+		public void OnNewMapLoaded(Game game) { }
+	}
+}
