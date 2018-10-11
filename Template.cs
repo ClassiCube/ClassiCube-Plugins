8e579bb0c53d1227b1f26a1ce8089ea6ad431ed0
UnknownShadow200 <unknownshadow200@gmail.com>
2018-10-12 08:28:16 +1100

    Create Template.cs

---
+++ Template.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+using ClassicalSharp;
+
+namespace PluginTemplate {
+	public sealed class TestPlugin : Plugin {
+		// Which version of the ClassicalSharp API this plugin is compatible with.
+		// Note that only plugins with the exact same API version as the client get loaded.
+		public int APIVersion { get { return 2; } }
+		// Called at game end (destroy native resources)
+		public void Dispose() { }
+		
+		// Called at game load
+		public void Init(Game game) {
+			this.game = game;
+        }
+
+		// Called at game load (after texture pack has been loaded)
+		public void Ready(Game game) { }
+		// Called when game has been reset (Player is reconnecting)
+		public void Reset(Game game) { }
+		// Called when player begins loading a new map
+		public void OnNewMap(Game game) { }
+		// Called after player finishes loading a new map
+		public void OnNewMapLoaded(Game game) { }
+	}
+}
