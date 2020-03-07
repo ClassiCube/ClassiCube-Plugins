c680e25fb30d6463e684984c1e4884eb42ca9920
UnknownShadow200 <unknownshadow200@gmail.com>
2020-03-08 10:44:39 +1100

    Fix ObjExporter with blocks over 256 and to actually compile again

--- src/ObjExporter.c
+++ src/ObjExporter.c
@@ -1,6 +1,12 @@
 // Since we are building an external plugin dll, we need to import from ClassiCube lib instead of exporting these
+#ifdef CC_BUILD_WIN
+// need to specifically declare as imported for Visual Studio
 #define CC_API __declspec(dllimport)
 #define CC_VAR __declspec(dllimport)
+#else
+#define CC_API
+#define CC_VAR
+#endif
 
 // NOTE !! Make sure you use the 'Release' configuration when building the dlls to give to end-users.
 // Check that the dlls are 25kb instead of debug dll 50-70 kb. (debug dlls depend on MSVCRT debug libs, which end users won't have)
@@ -8,14 +14,15 @@
 // The proper way would be to add 'additional include directories' and 'additional libs' in Visual Studio Project properties
 // Or, you can just be lazy and change these paths for your own system. 
 // You must compile ClassiCube in both x86 and x64 configurations to generate the .lib file.
-#include "H:/PortableApps/GitPortable/App/Git/ClassicalSharp/src/GameStructs.h"
-#include "H:/PortableApps/GitPortable/App/Git/ClassicalSharp/src/Block.h"
-#include "H:/PortableApps/GitPortable/App/Git/ClassicalSharp/src/ExtMath.h"
-#include "H:/PortableApps/GitPortable/App/Git/ClassicalSharp/src/Game.h"
-#include "H:/PortableApps/GitPortable/App/Git/ClassicalSharp/src/Chat.h"
-#include "H:/PortableApps/GitPortable/App/Git/ClassicalSharp/src/Stream.h"
-#include "H:/PortableApps/GitPortable/App/Git/ClassicalSharp/src/TexturePack.h"
-#include "H:/PortableApps/GitPortable/App/Git/ClassicalSharp/src/World.h"
+#include "GameStructs.h"
+#include "Block.h"
+#include "ExtMath.h"
+#include "Vectors.h"
+#include "Game.h"
+#include "Chat.h"
+#include "Stream.h"
+#include "TexturePack.h"
+#include "World.h"
 
 #ifdef _WIN64
 #pragma comment(lib, "H:/PortableApps/GitPortable/App/Git/ClassicalSharp/src/x64/Debug/ClassiCube.lib")
@@ -27,14 +34,14 @@
 /*########################################################################################################################*
 *--------------------------------------------------------Obj exporter-----------------------------------------------------*
 *#########################################################################################################################*/
-static bool all, mirror;
+static cc_bool all, mirror;
 static struct Stream stream;
-static bool include[BLOCK_COUNT]; // which blocks to actually dump vertices of
+static cc_bool include[BLOCK_COUNT]; // which blocks to actually dump vertices of
 static int texI[BLOCK_COUNT]; // index mapping for textures 
 // buffer data written
-static uint8_t buffer[17000];
+static cc_uint8  buffer[17000];
 static int bufferLen;
-#define Buffer_Str(str, len) str.buffer = &buffer[bufferLen]; str.length = 0; str.capacity = len;
+#define Buffer_Str(str, len) str.buffer = (char*)&buffer[bufferLen]; str.length = 0; str.capacity = len;
 
 static void Obj_Init(void) {
 	const static String invalid = String_FromConst("Invalid");
@@ -50,7 +57,7 @@ static void Obj_Init(void) {
 static void FlushData(int len) {
 	bufferLen += len;
 	if (bufferLen < 16384) return;
-	uint32_t modified;
+	cc_uint32 modified;
 
 	stream.Write(&stream, buffer, bufferLen, &modified);
 	bufferLen = 0;
@@ -67,7 +74,7 @@ static void WriteConst(const char* src) {
 	oneZ = width;      maxZ = World.MaxZ; length = World.Length;\
 	oneY = World.OneY; maxY = World.MaxY; height = World.Height;\
 	blocks = World.Blocks; blocks2 = World.Blocks2;\
-	mask = blocks == blocks2 ? 255 : 767;
+	mask = blocks == blocks2 ? 255 : 1023;
 
 static void DumpNormals(void) {
 	WriteConst("#normals\n");
@@ -113,10 +120,10 @@ static void DumpTextures() {
 		if (!include[b]) continue;
 		WriteTexName(b);
 
-		Vector3 min = Blocks.MinBB[b], max = Blocks.MaxBB[b];	
+		Vec3 min = Blocks.MinBB[b], max = Blocks.MaxBB[b];	
 		if (Blocks.Draw[b] == DRAW_SPRITE) {
-			min = Vector3_Zero();
-			max = Vector3_One();
+			Vec3_Set(min, 0,0,0);
+			Vec3_Set(max, 1,1,1);
 		}
 		texI[b] = i;
 
@@ -159,7 +166,7 @@ static void DumpTextures() {
 	}
 }
 
-static bool IsFaceHidden(int block, int other, int side) {
+static cc_bool IsFaceHidden(int block, int other, int side) {
 	return (Blocks.Hidden[(block * BLOCK_COUNT) + other] & (1 << side)) != 0;
 }
 
@@ -173,12 +180,12 @@ static RNGState spriteRng;
 static void DumpVertices() {
 	WriteConst("#vertices\n");
 	int i = -1, mask;
-	Vector3 min, max;
+	Vec3 min, max;
 
 	int oneX, maxX, width;
 	int oneY, maxY, height;
 	int oneZ, maxZ, length;
-	uint8_t *blocks, *blocks2;
+	cc_uint8 *blocks, *blocks2;
 	InitVars();
 
 	for (int y = 0; y < height; y++) {
@@ -236,8 +243,8 @@ static void DumpVertices() {
 					continue;
 				}
 
-				Vector3_AddBy(&min, &Blocks.RenderMinBB[b]);
-				Vector3_AddBy(&max, &Blocks.RenderMaxBB[b]);
+				Vec3_AddBy(&min, &Blocks.RenderMinBB[b]);
+				Vec3_AddBy(&max, &Blocks.RenderMaxBB[b]);
 
 				// minx
 				if (x == 0 || all || !IsFaceHidden(b, (blocks[i - oneX] | (blocks2[i - oneX] << 8)) & mask, FACE_XMIN)) {
@@ -307,7 +314,7 @@ static void DumpFaces() {
 	int oneX, maxX, width;
 	int oneY, maxY, height;
 	int oneZ, maxZ, length;
-	uint8_t *blocks, *blocks2;
+	cc_uint8 *blocks, *blocks2;
 	InitVars();
 
 	for (int y = 0; y < height; y++) {
@@ -369,7 +376,7 @@ static void ExportObj(void) {
 	DumpFaces();
 
 	if (!bufferLen) return;
-	uint32_t modified;
+	cc_uint32 modified;
 
 	stream.Write(&stream, buffer, bufferLen, &modified);
 	bufferLen = 0;
@@ -387,7 +394,7 @@ static void ObjExporterCommand_Execute(const String* args, int argsCount) {
 	String str = String_FromArray(strBuffer);
 	String_Format1(&str, "maps/%s.obj", &args[0]);
 
-	ReturnCode res = Stream_CreateFile(&stream, &str);
+	cc_result res = Stream_CreateFile(&stream, &str);
 	if (res) {
 		str.length = 0;
 		String_Format2(&str, "error %h creating maps/%s.obj", &res, &args[0]);
@@ -427,7 +434,15 @@ static void ObjExporter_Init(void) {
 /*########################################################################################################################*
 *----------------------------------------------------Plugin boilerplate---------------------------------------------------*
 *#########################################################################################################################*/
-__declspec(dllexport) int Plugin_ApiVersion = 1;
-__declspec(dllexport) struct IGameComponent Plugin_Component = {
+#ifdef CC_BUILD_WIN
+// special attribute to get symbols exported with Visual Studio
+#define PLUGIN_EXPORT __declspec(dllexport)
+#else
+// public symbols already exported when compiling shared lib with GCC
+#define PLUGIN_EXPORT
+#endif
+
+PLUGIN_EXPORT int Plugin_ApiVersion = 1;
+PLUGIN_EXPORT struct IGameComponent Plugin_Component = {
 	ObjExporter_Init /* Init */
 };
