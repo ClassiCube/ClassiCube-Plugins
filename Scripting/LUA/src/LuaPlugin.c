bd8410d93817751fbe71ddf95284b780e6cad34f
UnknownShadow200 <unknownshadow200@gmail.com>
2021-06-25 19:29:01 +1000

    Split up LUA plugin into lua plugin and common scripting.h, reorganise various folders around

--- Scripting/LUA/src/LuaPlugin.c
+++ Scripting/LUA/src/LuaPlugin.c
@@ -5,39 +5,33 @@
 #include "lua.h"
 #include "lualib.h"
 #include "lauxlib.h"
-
-// The proper way would be to add 'additional include directories' and 'additional libs' in Visual Studio Project properties
-// Or, you can just be lazy and change these paths for your own system. 
-// You must compile ClassiCube in both x86 and x64 configurations to generate the .lib file.
-#include "../../../ClassicalSharp/src/Game.h"
 #include "../../../ClassicalSharp/src/String.h"
-#include "../../../ClassicalSharp/src/Block.h"
-#include "../../../ClassicalSharp/src/ExtMath.h"
-#include "../../../ClassicalSharp/src/Chat.h"
-#include "../../../ClassicalSharp/src/Stream.h"
-#include "../../../ClassicalSharp/src/TexturePack.h"
-#include "../../../ClassicalSharp/src/World.h"
-#include "../../../ClassicalSharp/src/Funcs.h"
-#include "../../../ClassicalSharp/src/Event.h"
-#include "../../../ClassicalSharp/src/Server.h"
-#include "../../../ClassicalSharp/src/Window.h"
-
-#ifdef _WIN64
-#pragma comment(lib, "C:/GitPortable/Data/Home/ClassicalSharp/src/x64/Debug/ClassiCube.lib")
-#else
-#pragma comment(lib, "C:/GitPortable/Data/Home/ClassicalSharp/src/x86/Debug/ClassiCube.lib")
-#endif
-
-// ====== LUA BASE PLUGIN API ======
-struct LuaPlugin;
-typedef struct LuaPlugin { lua_State* L; struct LuaPlugin* next; } LuaPlugin;
-static LuaPlugin* pluginsHead;
 
 static cc_string LuaPlugin_GetString(lua_State* L, int idx) {
 	size_t len;
 	const char* msg = lua_tolstring(L, idx, &len);
 	return String_Init(msg, len, len);
 }
+
+#define SCRIPTING_DIRECTORY "lua"
+#define SCRIPTING_CONTEXT lua_State*
+#define SCRIPTING_RESULT int
+
+#define Scripting_GetString(ctx, arg) LuaPlugin_GetString(ctx, -(arg)-1)
+#define Scripting_GetInt(ctx, arg) lua_tointeger(ctx, -(arg)-1)
+#define Scripting_Consume(ctx, args) lua_pop(ctx, args)
+
+#define Scripting_ReturnVoid(ctx) return 0;
+#define Scripting_ReturnInt(ctx, value) lua_pushinteger(ctx, value); return 1;
+#define Scripting_ReturnBoolean(ctx, value) lua_pushboolean(ctx, value); return 1;
+#define Scripting_ReturnString(ctx, buffer, len) lua_pushlstring(ctx, buffer, len); return 1;
+
+#include "Scripting.h"
+
+// ====== LUA BASE PLUGIN API ======
+struct LuaPlugin;
+typedef struct LuaPlugin { lua_State* L; struct LuaPlugin* next; } LuaPlugin;
+static LuaPlugin* pluginsHead;
 
 static void LuaPlugin_LogError(lua_State* L, const char* place, const void* arg1, const void* arg2) {
 	char buffer[256];
@@ -57,7 +51,6 @@ static void LuaPlugin_LogError(lua_State* L, const char* place, const void* arg1
 	Chat_Add(&str);
 }
 
-
 // macro to avoid verbose code duplication
 #define LuaPlugin_RaiseCommonBegin \
 	LuaPlugin* plugin = pluginsHead;\
@@ -75,14 +68,14 @@ static void LuaPlugin_LogError(lua_State* L, const char* place, const void* arg1
 		plugin = plugin->next;\
 	}
 
-static void LuaPlugin_RaiseVoid(const char* groupName, const char* funcName) {
+static void Scripting_RaiseVoid(const char* groupName, const char* funcName) {
 	LuaPlugin_RaiseCommonBegin
 		int ret = lua_pcall(L, 0, 0, 0); /* call implicitly pops function */
 		if (ret) LuaPlugin_LogError(L, "running callback", groupName, funcName);
 	LuaPlugin_RaiseCommonEnd
 }
 
-static void LuaPlugin_RaiseChat(const char* groupName, const char* funcName, const cc_string* msg, int msgType) {
+static void Scripting_RaiseChat(const char* groupName, const char* funcName, const cc_string* msg, int msgType) {
 	LuaPlugin_RaiseCommonBegin
 		lua_pushlstring(L, msg->buffer, msg->length);
 		lua_pushinteger(L, msgType);
@@ -95,66 +88,16 @@ static void LuaPlugin_RaiseChat(const char* groupName, const char* funcName, con
 /*########################################################################################################################*
 *--------------------------------------------------------Chat api---------------------------------------------------------*
 *#########################################################################################################################*/
-static int CC_Chat_Add(lua_State* L) {
-	cc_string str = LuaPlugin_GetString(L, -1);
-	Chat_Add(&str);
-	lua_pop(L, 1);
-	return 0;
-}
-
-static int CC_Chat_Send(lua_State* L) {
-	cc_string str = LuaPlugin_GetString(L, -1);
-	Chat_Send(&str, false);
-	lua_pop(L, 1);
-	return 0;
-}
-
 static const struct luaL_Reg chatFuncs[] = {
 	{ "add",  CC_Chat_Add },
 	{ "send", CC_Chat_Send },
 	{ NULL, NULL }
 };
 
-static void CC_Chat_OnReceived(void* obj, const cc_string* msg, int msgType) {
-	LuaPlugin_RaiseChat("chat", "onReceived", msg, msgType);
-}
-static void CC_Chat_OnSent(void* obj, const cc_string* msg, int msgType) {
-	LuaPlugin_RaiseChat("chat", "onSent", msg, msgType);
-}
-static void CC_Chat_Hook(void) {
-	Event_Register_(&ChatEvents.ChatReceived, NULL, CC_Chat_OnReceived);
-	Event_Register_(&ChatEvents.ChatSending,  NULL, CC_Chat_OnSent);
-}
-
 
 /*########################################################################################################################*
 *-------------------------------------------------------Server api--------------------------------------------------------*
 *#########################################################################################################################*/
-static int CC_Server_GetMotd(lua_State* L) {
-	lua_pushlstring(L, Server.MOTD.buffer, Server.MOTD.length);
-	return 1;
-}
-static int CC_Server_GetName(lua_State* L) {
-	lua_pushlstring(L, Server.Name.buffer, Server.Name.length);
-	return 1;
-}
-static int CC_Server_GetAppName(lua_State* L) {
-	lua_pushlstring(L, Server.AppName.buffer, Server.AppName.length);
-	return 1;
-}
-
-static int CC_Server_SetAppName(lua_State* L) {
-	cc_string str = LuaPlugin_GetString(L, -1);
-	String_Copy(&Server.AppName, &str);
-	lua_pop(L, 1);
-	return 0;
-}
-
-static int CC_Server_IsSingleplayer(lua_State* L) {
-	lua_pushboolean(L, Server.IsSinglePlayer);
-	return 1;
-}
-
 static int CC_Server_SendData(lua_State* L) {
 	cc_uint8* data;
 	size_t len;
@@ -191,44 +134,10 @@ static const struct luaL_Reg serverFuncs[] = {
 	{ NULL, NULL }
 };
 
-static void CC_Server_OnConnected(void* obj) {
-	LuaPlugin_RaiseVoid("server", "onConnected");
-}
-static void CC_Server_OnDisconnected(void* obj) {
-	LuaPlugin_RaiseVoid("server", "onDisconnected");
-}
-static void CC_Server_Hook(void) {
-	Event_Register_(&NetEvents.Connected,    NULL, CC_Server_OnConnected);
-	Event_Register_(&NetEvents.Disconnected, NULL, CC_Server_OnDisconnected);
-}
-
 
 /*########################################################################################################################*
 *--------------------------------------------------------World api--------------------------------------------------------*
 *#########################################################################################################################*/
-static int CC_World_GetWidth(lua_State* L) {
-	lua_pushinteger(L, World.Width);
-	return 1;
-}
-static int CC_World_GetHeight(lua_State* L) {
-	lua_pushinteger(L, World.Height);
-	return 1;
-}
-static int CC_World_GetLength(lua_State* L) {
-	lua_pushinteger(L, World.Length);
-	return 1;
-}
-
-static int CC_World_GetBlock(lua_State* L) {
-	int x = lua_tointeger(L, -3);
-	int y = lua_tointeger(L, -2);
-	int z = lua_tointeger(L, -1);
-
-	lua_pop(L, 3);
-	lua_pushinteger(L, World_GetBlock(x, y, z));
-	return 1;
-}
-
 static const struct luaL_Reg worldFuncs[] = {
 	{ "getWidth",  CC_World_GetWidth  },
 	{ "getHeight", CC_World_GetHeight },
@@ -237,28 +146,10 @@ static const struct luaL_Reg worldFuncs[] = {
 	{ NULL, NULL }
 };
 
-static void CC_World_OnNew(void* obj) {
-	LuaPlugin_RaiseVoid("world", "onNewMap");
-}
-static void CC_World_OnMapLoaded(void* obj) {
-	LuaPlugin_RaiseVoid("world", "onMapLoaded");
-}
-static void CC_World_Hook(void) {
-	Event_Register_(&WorldEvents.NewMap,    NULL, CC_World_OnNew);
-	Event_Register_(&WorldEvents.MapLoaded, NULL, CC_World_OnMapLoaded);
-}
-
 
 /*########################################################################################################################*
 *--------------------------------------------------------Window api-------------------------------------------------------*
 *#########################################################################################################################*/
-static int CC_Window_SetTitle(lua_State* L) {
-	cc_string str = LuaPlugin_GetString(L, -1);
-	Window_SetTitle(&str);
-	lua_pop(L, 1);
-	return 0;
-}
-
 static const struct luaL_Reg windowFuncs[] = {
 	{ "setTitle", CC_Window_SetTitle },
 	{ NULL, NULL }
@@ -282,7 +173,7 @@ static lua_State* LuaPlugin_New(void) {
 	return L;
 }
 
-static void LuaPlugin_Load(const cc_string* origName, void* obj) {
+static void Backend_Load(const cc_string* origName, void* obj) {
 	static cc_string ext = String_FromConst(".lua");
 	if (!String_CaselessEnds(origName, &ext)) return;
 	cc_string name; char nameBuffer[601];
@@ -345,19 +236,6 @@ static struct ChatCommand LuaPlugin_Cmd = {
 	}
 };
 
-static void LuaPlugin_Init(void) {
-	const static cc_string luaDir = String_FromConst("lua");
-	Directory_Create(&luaDir);
-
-	Directory_Enum(&luaDir, NULL, LuaPlugin_Load);
+static void Backend_Init(void) {
 	Commands_Register(&LuaPlugin_Cmd);
-
-	CC_Chat_Hook();
-	CC_Server_Hook();
-	CC_World_Hook();
 }
-
-__declspec(dllexport) int Plugin_ApiVersion = 1;
-__declspec(dllexport) struct IGameComponent Plugin_Component = {
-	LuaPlugin_Init /* Init */
-};
---
+++ Scripting/Scripting.h
@@ -0,0 +1,222 @@
+// NOTE: Make sure the Scripting.h files in the various backend folders
+//  are kept up to date with the Scripting.h file in the root folder
+// ====================================================================
+
+// Since we are building an external plugin dll, we need to import from ClassiCube lib instead of exporting these
+#define CC_API __declspec(dllimport)
+#define CC_VAR __declspec(dllimport)
+
+// The proper way would be to add 'additional include directories' and 'additional libs' in Visual Studio Project properties
+// Or, you can just be lazy and change these paths for your own system. 
+// You must compile ClassiCube in both x86 and x64 configurations to generate the .lib file.
+#include "../../../ClassicalSharp/src/Game.h"
+#include "../../../ClassicalSharp/src/String.h"
+#include "../../../ClassicalSharp/src/Block.h"
+#include "../../../ClassicalSharp/src/ExtMath.h"
+#include "../../../ClassicalSharp/src/Chat.h"
+#include "../../../ClassicalSharp/src/Stream.h"
+#include "../../../ClassicalSharp/src/TexturePack.h"
+#include "../../../ClassicalSharp/src/World.h"
+#include "../../../ClassicalSharp/src/Funcs.h"
+#include "../../../ClassicalSharp/src/Event.h"
+#include "../../../ClassicalSharp/src/Server.h"
+#include "../../../ClassicalSharp/src/Window.h"
+
+#ifdef _WIN64
+#pragma comment(lib, "../../../ClassicalSharp/src/x64/Debug/ClassiCube.lib")
+#else
+#pragma comment(lib, "../../../ClassicalSharp/src/x86/Debug/ClassiCube.lib")
+#endif
+
+static void Scripting_RaiseVoid(const char* groupName, const char* funcName);
+static void Scripting_RaiseChat(const char* groupName, const char* funcName, const cc_string* msg, int msgType);
+
+static void Backend_Load(const cc_string* origName, void* obj);
+static void Backend_Init(void);
+
+/*
+#define SCRIPTING_DIRECTORY
+#define SCRIPTING_CONTEXT
+
+#define Scripting_GetString(SCRIPTING_CONTEXT, arg)
+#define Scripting_GetInt(SCRIPTING_CONTEXT, arg)
+#define Scripting_Consume(SCRIPTING_CONTEXT, args)
+
+#define SCRIPTING_RESULT
+#define Scripting_ReturnVoid(SCRIPTING_CONTEXT)
+#define Scripting_ReturnInt(SCRIPTING_CONTEXT, value)
+#define Scripting_ReturnBoolean(SCRIPTING_CONTEXT, value)
+#define Scripting_ReturnString(SCRIPTING_CONTEXT, buffer, len)
+*/
+
+
+/*########################################################################################################################*
+*--------------------------------------------------------Chat api---------------------------------------------------------*
+*#########################################################################################################################*/
+static SCRIPTING_RESULT CC_Chat_Add(SCRIPTING_CONTEXT ctx) {
+	cc_string str = Scripting_GetString(ctx, 0);
+	Chat_Add(&str);
+
+	Scripting_Consume(ctx, 1);
+	Scripting_ReturnVoid(ctx);
+}
+
+static SCRIPTING_RESULT CC_Chat_Send(SCRIPTING_CONTEXT ctx) {
+	cc_string str = Scripting_GetString(ctx, 0);
+	Chat_Send(&str, false);
+
+	Scripting_Consume(ctx, 1);
+	Scripting_ReturnVoid(ctx);
+}
+
+static void CC_Chat_OnReceived(void* obj, const cc_string* msg, int msgType) {
+	Scripting_RaiseChat("chat", "onReceived", msg, msgType);
+}
+static void CC_Chat_OnSent(void* obj, const cc_string* msg, int msgType) {
+	Scripting_RaiseChat("chat", "onSent", msg, msgType);
+}
+static void CC_Chat_Hook(void) {
+	Event_Register_(&ChatEvents.ChatReceived, NULL, CC_Chat_OnReceived);
+	Event_Register_(&ChatEvents.ChatSending,  NULL, CC_Chat_OnSent);
+}
+
+
+/*########################################################################################################################*
+*-------------------------------------------------------Server api--------------------------------------------------------*
+*#########################################################################################################################*/
+static SCRIPTING_RESULT CC_Server_GetMotd(SCRIPTING_CONTEXT ctx) {
+	Scripting_ReturnString(ctx, Server.MOTD.buffer, Server.MOTD.length);
+}
+static SCRIPTING_RESULT CC_Server_GetName(SCRIPTING_CONTEXT ctx) {
+	Scripting_ReturnString(ctx, Server.Name.buffer, Server.Name.length);
+}
+static SCRIPTING_RESULT CC_Server_GetAppName(SCRIPTING_CONTEXT ctx) {
+	Scripting_ReturnString(ctx, Server.AppName.buffer, Server.AppName.length);
+}
+
+static SCRIPTING_RESULT CC_Server_SetAppName(SCRIPTING_CONTEXT ctx) {
+	cc_string str = Scripting_GetString(ctx, 0);
+	String_Copy(&Server.AppName, &str);
+
+	Scripting_Consume(ctx, 1);
+	Scripting_ReturnVoid(ctx);
+}
+
+static SCRIPTING_RESULT CC_Server_IsSingleplayer(SCRIPTING_CONTEXT ctx) {
+	Scripting_ReturnBoolean(ctx, Server.IsSinglePlayer);
+}
+
+/* this one is too tricky to abstract */
+/*static SCRIPTING_RESULT CC_Server_SendData(lua_State* L) {
+	cc_uint8* data;
+	size_t len;
+	int i, type = lua_type(L, -1);
+
+	if (type == LUA_TSTRING) {
+		data = lua_tolstring(L, -1, &len);
+		Server.SendData(data, len);
+	} else if (type == LUA_TTABLE) {
+		len  = lua_rawlen(L, -1);
+		data = Mem_Alloc(len, 1, "lua send data");
+
+		for (i = 1; i <= len; i++) {
+			lua_rawgeti(L, -1, i);
+			data[i - 1] = lua_tointeger(L, -1);
+			lua_pop(L, 1);
+		}
+
+		Server.SendData(data, len);
+		Mem_Free(data);
+	} else {
+		luaL_error(L, "data must be string or an array table");
+	}
+	return 0;
+}*/
+
+static void CC_Server_OnConnected(void* obj) {
+	Scripting_RaiseVoid("server", "onConnected");
+}
+static void CC_Server_OnDisconnected(void* obj) {
+	Scripting_RaiseVoid("server", "onDisconnected");
+}
+static void CC_Server_Hook(void) {
+	Event_Register_(&NetEvents.Connected,    NULL, CC_Server_OnConnected);
+	Event_Register_(&NetEvents.Disconnected, NULL, CC_Server_OnDisconnected);
+}
+
+
+/*########################################################################################################################*
+*--------------------------------------------------------World api--------------------------------------------------------*
+*#########################################################################################################################*/
+static SCRIPTING_RESULT CC_World_GetWidth(SCRIPTING_CONTEXT ctx) {
+	Scripting_ReturnInt(ctx, World.Width);
+}
+
+static SCRIPTING_RESULT CC_World_GetHeight(SCRIPTING_CONTEXT ctx) {
+	Scripting_ReturnInt(ctx, World.Height);
+}
+
+static SCRIPTING_RESULT CC_World_GetLength(SCRIPTING_CONTEXT ctx) {
+	Scripting_ReturnInt(ctx, World.Length);
+}
+
+static SCRIPTING_RESULT CC_World_GetBlock(SCRIPTING_CONTEXT ctx) {
+	int x = Scripting_GetInt(ctx, 2);
+	int y = Scripting_GetInt(ctx, 1);
+	int z = Scripting_GetInt(ctx, 0);
+
+	Scripting_Consume(ctx, 3);
+	Scripting_ReturnInt(ctx, World_GetBlock(x, y, z));
+}
+
+static void CC_World_OnNew(void* obj) {
+	Scripting_RaiseVoid("world", "onNewMap");
+}
+static void CC_World_OnMapLoaded(void* obj) {
+	Scripting_RaiseVoid("world", "onMapLoaded");
+}
+static void CC_World_Hook(void) {
+	Event_Register_(&WorldEvents.NewMap,    NULL, CC_World_OnNew);
+	Event_Register_(&WorldEvents.MapLoaded, NULL, CC_World_OnMapLoaded);
+}
+
+
+/*########################################################################################################################*
+*--------------------------------------------------------Window api-------------------------------------------------------*
+*#########################################################################################################################*/
+static SCRIPTING_RESULT CC_Window_SetTitle(SCRIPTING_CONTEXT ctx) {
+	cc_string str = Scripting_GetString(ctx, 0);
+	Window_SetTitle(&str);
+
+	Scripting_Consume(ctx, 1);
+	Scripting_ReturnVoid(ctx);
+}
+
+
+/*########################################################################################################################*
+*-------------------------------------------------Plugin implementation---------------------------------------------------*
+*#########################################################################################################################*/
+static void Scripting_Init(void) {
+	const static cc_string dir = String_FromConst(SCRIPTING_DIRECTORY);
+	Directory_Create(&dir);
+
+	Directory_Enum(&dir, NULL, Backend_Load);
+	Backend_Init();
+
+	CC_Chat_Hook();
+	CC_Server_Hook();
+	CC_World_Hook();
+}
+
+#ifdef CC_BUILD_WIN
+// special attribute to get symbols exported on Windows
+#define PLUGIN_EXPORT __declspec(dllexport)
+#else
+// public symbols already exported when compiling shared lib with GCC
+#define PLUGIN_EXPORT
+#endif
+
+PLUGIN_EXPORT int Plugin_ApiVersion = 1;
+PLUGIN_EXPORT struct IGameComponent Plugin_Component = {
+	Scripting_Init /* Init */
+};
